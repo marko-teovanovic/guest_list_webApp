2ba9329476c4cb73e9c439fbdba9fc32ed3acac4
DESKTOP-JAFFL6S\MiM <galama92marko@gmail.com>
2023-12-20 12:56:17 +0100

    changing structure of project

---
+++ frontend/src/components/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>
+      <h1>GUEST LIST</h1>
+      <Link to="/create">
+        <button className='Create'>Create new</button>
+      </Link>
+      <Link to="/all">
+        <button className='All'>See all lists</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Home
